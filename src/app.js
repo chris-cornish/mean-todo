5f605b26fbf3912dd722ff72c564e5845f0bce6d
chris-cornish <csquare2390@gmail.com>
2017-01-03 13:19:26 -0500

    Addes todos route

--- src/app.js
+++ src/app.js
@@ -6,6 +6,10 @@ var app = express();
 
 app.use('/', express.static('public'));
 
+app.get('/todos', function(req, res) {
+  res.send("These are the todos!");
+});
+
 app.listen(3000, function() {
   console.log("The server is running on port 3000!");
 });
